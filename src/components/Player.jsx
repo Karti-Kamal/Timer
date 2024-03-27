7c0be825d93cc7049c876c90fcfb8dbd2b7a3e65
Karti Kamal <kamal.k.karti@gmail.com>
2024-03-27 14:36:15 +0000

    Timer init

---
+++ src/components/Player.jsx
@@ -0,0 +1,21 @@
+import { useRef } from "react";
+import { useState } from "react";
+
+export default function Player() {
+  const playerName = useRef();
+  const [entredPlayerName, setEntredPlayerName] = useState(null);
+
+  function handleClick() {
+    setEntredPlayerName(playerName.current.value);
+    playerName.current.value = '';
+  }
+  return (
+    <section id="player">
+      <h2>Welcome {entredPlayerName ?? "unknown entity"}</h2>
+      <p>
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
